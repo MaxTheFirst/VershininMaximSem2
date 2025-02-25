f3c292d37b110fd2b240eb3ff3ed931352bc8500
MaxTheFirst <maks.vershinin.2005@mail.ru>
2025-02-26 01:34:59 +0300

    edit data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,7 @@
--- Для таблицы категорий
 INSERT INTO categories (category_id, name) VALUES (1, 'Technology');
 INSERT INTO categories (category_id, name) VALUES (2, 'Health');
 INSERT INTO categories (category_id, name) VALUES (3, 'Education');
 
--- Для таблицы сайтов
 INSERT INTO websites (website_id, name, url) VALUES (1, 'Google', 'https://www.google.com');
 INSERT INTO websites (website_id, name, url) VALUES (2, 'Wikipedia', 'https://www.wikipedia.org');
 INSERT INTO websites (website_id, name, url) VALUES (3, 'StackOverflow', 'https://stackoverflow.com');
